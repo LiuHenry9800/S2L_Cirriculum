21f05e362003113138b64454dff5e01af582f29e
LiuHenry9800 <henryliu9800@gmail.com>
2025-12-02 02:31:26 -0800

    feat: log files

--- run.sh
+++ run.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 echo "Starting avg data selection"
-python TRL/selection/data_selection.py --config TRL/configs/selection/avg.yml
+python TRL/selection/data_selection.py --config TRL/configs/selection/avg.yml > avg_select.log 2>&1 
 
 
 echo "Starting overall data selection"
-python TRL/selection/data_selection.py --config TRL/configs/selection/overall.yml
+python TRL/selection/data_selection.py --config TRL/configs/selection/overall.yml> ovr_select.log 2>&1 
 
 
 
 echo "Starting 410M Avg Training"
-python TRL/Train.py --config TRL/configs/410M_Avg.yml
+python TRL/Train.py --config TRL/configs/410M_Avg.yml> avg_train.log 2>&1 
 
 echo "Starting 410M Overall Training"
-python TRL/Train.py --config TRL/configs/410M_Overall.yml
+python TRL/Train.py --config TRL/configs/410M_Overall.yml > ovr_train.log 2>&1 
 
 
 
