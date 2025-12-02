01573ab41a596c4fefed3368344e9a0ae7471f16
LiuHenry9800 <henryliu9800@gmail.com>
2025-12-02 02:30:23 -0800

    feat: new run script

--- run.sh
+++ run.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
-echo "Starting 70M Full Training"
-python TRL/Train.py --config TRL/configs/70M_Full.yml
+echo "Starting avg data selection"
+python TRL/selection/data_selection.py --config TRL/configs/selection/avg.yml
 
-echo "Starting 410M Full Training"
-python TRL/Train.py --config TRL/configs/410M_Full.yml
+
+echo "Starting overall data selection"
+python TRL/selection/data_selection.py --config TRL/configs/selection/overall.yml
+
+
+
+echo "Starting 410M Avg Training"
+python TRL/Train.py --config TRL/configs/410M_Avg.yml
+
+echo "Starting 410M Overall Training"
+python TRL/Train.py --config TRL/configs/410M_Overall.yml
+
+
+
+
+
